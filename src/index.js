d16d64faf69a8121eaa1e1016cf6f700cca576ef
nsymester <freal7@yahoo.com>
2018-12-14 01:05:20 +0000

    Configure babel for React with preset-react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import getGreeting from './greet';
+// import getGreeting from './greet';
+// console.log(getGreeting('world'));
 
-console.log(getGreeting('world'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
