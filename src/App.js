049958965251dbb13a389f6496ba797881f7cdd1
nsymester <freal7@yahoo.com>
2018-12-14 16:12:59 +0000

    Hot Reload a React App in Development with react-hot-loader, Avoid Duplicate Commands by Calling one NPM Script from Another

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {hot} from 'react-hot-loader';
 
 class App extends React.Component {
   state = {
@@ -7,10 +8,13 @@ class App extends React.Component {
 
   render() {
     // debugger
+    const {count} = this.state;
     return (
       <div>
         <h1>Hello world!!</h1>
-        <h2>Count: {this.state.count}</h2>
+        <h2 className={count > 10 ? 'warning': null}>
+          Count: {count}
+        </h2>
         <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
         <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
       </div>
@@ -18,4 +22,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
